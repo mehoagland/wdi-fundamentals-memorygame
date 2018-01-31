0446b8114b6b4d02ceb1d15eca05c3a817b43761
ME Hoagland <mehoagland@MEs-MacBook-Pro.local>
2018-01-30 19:50:11 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen","king","king"];
+var cardsInPlay = [];
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if(cardsInPlay.length === 2){
+	if (cardOne === cardTwo){
+		alert("You found a match!");
+	}else {
+		alert("Sorry, try again.");
+	}
+} 
