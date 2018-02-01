bb6abf6b4efb77adae325d1ce855158aff23daf7
ME Hoagland <mehoagland@MEs-MacBook-Pro.local>
2018-02-01 18:31:21 -0500

    created gmae logic

--- js/main.js
+++ js/main.js
@@ -2,12 +2,6 @@
 var cards = [
 {
 rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
-},
-
-{
-rank: "queen",
 suit: "diamonds",
 cardImage: "images/queen-of-diamonds.png"
 },
@@ -16,6 +10,12 @@ cardImage: "images/queen-of-diamonds.png"
 rank: "king",
 suit: "hearts",
 cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
 },
 
 {
@@ -35,15 +35,27 @@ var checkForMatch = function(){
 	  }
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank)
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 	} 
 }
 
-flipCard(0);
-flipCard(1);
+var createBoard = function(){
+	for (let i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+
+createBoard();
